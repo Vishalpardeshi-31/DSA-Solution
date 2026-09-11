0671234f89cc9441dfd08d0c66dba3b224fa21f0
Vishal Pardeshi <vishalpardeshi962@gmail.com>
2026-09-11 23:31:34 +0530

    Time: 0 ms (100%), Space: 43 MB (19.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int prev = 0; 
-        int cur = 0;  
+        int abhi = 0;  
         
         for (int num : nums) {
-            int temp = Math.max(cur, prev + num);
-            prev = cur;
-            cur = temp;
+            int temp = Math.max(abhi, prev + num);
+            prev = abhi;
+            abhi = temp;
         }
         
-        return cur;
+        return abhi;
     }
 }   
